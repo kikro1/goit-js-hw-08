ad4377d9ed0cc7b8f16da99d29fa3ed332cb5915
kikro1 <bogdash.voitencov.2008@gmail.com>
2024-01-29 14:48:48 +0000

    basic

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,69 +1,117 @@
+// gallery.js
+
+// Масив об’єктів із зображеннями
 const images = [
-    {
-      preview: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
-      original: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
-      description: "Hokkaido Flower",
-    },
-    {
-      preview: "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-      original: "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-      description: "Container Haulage Freight",
-    },
-    {
-      preview: "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-      original: "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-      description: "Aerial Beach View",
-    },
-    {
-      preview: "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-      original: "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-      description: "Flower Blooms",
-    },
-    {
-      preview: "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-      original: "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-      description: "Alpine Mountains",
-    },
-    {
-      preview: "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-      original: "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-      description: "Mountain Lake Sailing",
-    },
-    {
-      preview: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-      original: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-      description: "Alpine Spring Meadows",
-    },
-    {
-      preview: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-      original: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-      description: "Nature Landscape",
-    },
-    {
-      preview: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-      original: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-      description: "Lighthouse Coast Sea",
-    },
-  ];
-  
-  const galleryContainer = document.querySelector('.gallery');
-  
-  images.forEach(image => {
-    const galleryItem = document.createElement('li');
-    galleryItem.classList.add('gallery-item');
-  
-    const link = document.createElement('a');
-    link.classList.add('gallery-link');
-    link.href = image.original;
-  
-    const img = document.createElement('img');
-    img.classList.add('gallery-image');
-    img.src = image.preview;
-    img.setAttribute('data-source', image.original);
-    img.alt = image.description;
-  
-    link.appendChild(img);
-    galleryItem.appendChild(link);
-    galleryContainer.appendChild(galleryItem);
-  });
-  
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
+
+// Отримайте посилання на елемент ul.gallery
+const galleryContainer = document.querySelector('.gallery');
+
+// Додайте розмітку елементів галереї до ul.gallery
+const galleryMarkup = createGalleryMarkup(images);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Додайте прослуховувач подій до контейнера галереї
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+// Функція створення розмітки елемента галереї
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `
+    )
+    .join('');
+}
+
+// Функція-обробник події кліку на елементі галереї
+function onGalleryItemClick(event) {
+  // Зупиніть стандартну поведінку посилань
+  event.preventDefault();
+
+  // Перевірте, чи клікнули саме на елемент зображення
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  // Отримайте посилання на велике зображення з data-атрибуту source
+  const largeImageURL = event.target.dataset.source;
+
+  // Виведіть URL великого зображення у консоль
+  console.log('Large Image URL:', largeImageURL);
+
+  // Тут ви можете використовувати посилання для відкриття модального вікна або виконати інші дії
+}
